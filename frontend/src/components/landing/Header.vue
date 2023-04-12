c0749d8d3eef1ad26e7dac509789846251b8062c
stivendiaz <stivendiazagudelo@gmail.com>
2023-04-12 15:47:20 -0500

    fix: changed style of header

--- frontend/src/components/landing/Header.vue
+++ frontend/src/components/landing/Header.vue
@@ -14,44 +14,79 @@ const toggle = (shouldOpen: boolean) => {
 </script>
 
 <template>
-    <header class="shadow-md">
-        <nav class="bg-white border-gray-200">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/" class="flex items-center">
-                    <img src="assets/logo.png" alt="logo" class="w-[180px] ml-10">
-                </a>
+  <!--
+  This component uses @tailwindcss/forms
 
-                <button 
-                data-collapse-handleOpenOrCloseSidebar="mobile-menu-2" 
-                type="button" 
-                class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" 
-                aria-controls="mobile-menu-2" 
-                aria-expanded="false"
-                @click.prevent="toggle(true)"
-                >
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                </button>
+  yarn add @tailwindcss/forms
+  npm install @tailwindcss/forms
 
-                <div class="items-center justify-end hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white mr-10">
-                        <li>
-                            <a class="text-[#502A18] block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 cursor-pointer hover:scale-110 transition-all" aria-current="page">Hostels</a>
-                        </li>
-                        <li v-if="!isLoggedIn">
-                            <a class="text-[#502A18] block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 cursor-pointer hover:scale-110 transition-all" aria-current="page">Signin</a>
-                        </li>
-                        <li v-if="!isLoggedIn">
-                            <a class="text-orange-600 block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 cursor-pointer hover:scale-110 transition-all" aria-current="page">Login</a>
-                        </li>
-                    </ul>
-                    <UserCircleIcon v-if="isLoggedIn" class="h-10 w-10 text-[#502A18]"  />
-                </div>
-            </div>
-        </nav>
-        <SideBarDrawer :open="open" @closeModal="toggle(false)">
-            <p>HOLAA</p>
-        </SideBarDrawer>
-    </header>
+  plugins: [require('@tailwindcss/forms')]
+-->
+
+<header aria-label="Site Header" class="shadow-md h-[80px]">
+  <div
+    class="mx-auto flex max-w-screen-xl items-center justify-between px-4 h-full"
+  >
+    <div class="flex w-0 flex-1 lg:hidden">
+      <button class="rounded-full bg-gray-100 p-2 text-gray-600" type="button">
+        <span class="sr-only">Account</span>
+        <svg
+          class="h-5 w-5"
+          fill="none"
+          stroke="currentColor"
+          viewbox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          ></path>
+        </svg>
+      </button>
+    </div>
+
+    <div class="flex items-center gap-4">
+      <a href="#">
+        <span class="sr-only">Logo</span>
+        <span class="h-10 w-20 rounded-lg bg-gray-200"></span>
+      </a>
+
+      <form class="mb-0 hidden lg:flex">
+        <div class="relative">
+          <img src="assets/logo.png" class="w-[200px]">
+        </div>
+      </form>
+    </div>
+
+    <nav
+      aria-label="Site Nav"
+      class="hidden items-center justify-center gap-8 text-sm font-medium lg:flex lg:w-0 lg:flex-1"
+    >
+      <a class="text-[#502A18] scale-110 transition-all" href="">Hostels</a>
+      <a class="text-[#502A18] scale-110 transition-all" href="">About</a>
+      <a class="text-[#502A18] scale-110 transition-all" href="">Contact</a>
+    </nav>
+
+    <div class="hidden items-center gap-4 lg:flex">
+      <a
+        href="#"
+        class="rounded-lg bg-gray-100 px-5 py-2 text-sm font-medium text-gray-600"
+      >
+        Log in
+      </a>
+
+      <a
+        href="#"
+        class="rounded-lg bg-orange-600 hover:bg-orange-400 px-5 py-2 text-sm font-medium text-white transition-all"
+      >
+        Sign up
+      </a>
+    </div>
+  </div>
+
+</header>
+
 </template>
 
