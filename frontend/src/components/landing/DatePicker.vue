78e71a7a01357b8bdcfc32922604e872016ced9c
stivendiaz <stivendiazagudelo@gmail.com>
2023-04-14 17:46:00 -0500

    feat(search bar): getting values from the search bar, changed styles of date picker, fixed animations

--- frontend/src/components/landing/DatePicker.vue
+++ frontend/src/components/landing/DatePicker.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-    import { ref, Ref, onMounted } from 'vue'
+    import { ref, watchEffect, defineEmits } from 'vue'
     import { DatePicker }  from 'v-calendar'
-    import moment from 'moment'
+    import VerticalIcon from '../../custom-icons/VerticalDiv.vue'
     import { CalendarDaysIcon,ArrowUpIcon, ArrowDownIcon } from '@heroicons/vue/24/solid'
     import 'v-calendar/dist/style.css'
 
@@ -10,20 +10,26 @@
         end: new Date(),
     });
 
+    const emits = defineEmits(['updateDatedRange'])
+    
+    watchEffect(() => {
+        emits('updateDatedRange', range.value);
+    })
 </script>
  
 <template>
-    <DatePicker v-model="range" :columns="2" :min-date="new Date()" is-range color="orange">
+    <DatePicker v-model="range" :columns="2" :min-date="new Date()" is-range color="orange" >
         <template #default="{ inputEvents, inputValue }">
-            <div class="group flex divide-x">
-                <div class="w-[50%] flex">
+            <div class="group flex justify-evenly items-center">
+                <div class="w-[35%] flex">
                     <label class="button groupItem flex" for="start">
                             <CalendarDaysIcon class="h-6 w-6 text-orange-600"/>
                             <ArrowUpIcon class="h-6 w-4 text-orange-600"/>
                     </label>
                     <input :value="inputValue.start" v-on="inputEvents.start" class="w-[70%] md:flex xs:hidden xs:text-xs lg:text-base text-gray-500" groupItem>
                 </div>
-                <div class="w-[50%] flex">
+                <VerticalIcon />
+                <div class="w-[35%] flex">
                     <label class="button groupItem flex" for="end">
                         <CalendarDaysIcon class="h-6 w-6 text-orange-600"/>
                         <ArrowDownIcon class="h-6 w-4 text-orange-600"/>
--- frontend/src/components/landing/SearchBar.vue
+++ frontend/src/components/landing/SearchBar.vue
@@ -1,32 +1,63 @@
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { Ref, ref, watchEffect } from 'vue';
 import { MagnifyingGlassIcon, UserPlusIcon, GlobeAsiaAustraliaIcon } from '@heroicons/vue/24/outline'
-import { UserCircleIcon, PlusIcon, MinusIcon } from '@heroicons/vue/24/solid'
+import { PlusIcon, MinusIcon } from '@heroicons/vue/24/solid'
 import ModalSmall from '../../common/ModalSmall.vue';
 import DatePicker from './DatePicker.vue'
+
+interface DatesRange {
+    start: string;
+    end: string;
+}
+interface Payload {
+    dates: DatesRange;
+    guests: number;
+    location: string;
+}
 
 enum AddOrSubtractGuest {
     ADD = 'add',
     SUBTRACT = 'subtract'
 }
-let numberOfGuests = ref(0)
-let modalIsOpen = ref(false);
+
+const payload: Ref<Payload> = ref({
+    guests: 0,
+    dates: {
+        start: '',
+        end: ''
+    },
+    location: ''
+})
+
+watchEffect(() => {
+    console.log(payload.value);
+})
+
+const modalIsOpen = ref(false);
+
+const updateDatedRange = (range: DatesRange) => {
+    payload.value.dates = range
+}
 
 const handleShowOrHideModal = (show: boolean) => {
-    console.log("OPEN");
     modalIsOpen.value = show
 }
 
 const handleAddOrSubtractGuest = (operation: string) => {
-    numberOfGuests.value = operation === AddOrSubtractGuest.ADD ? 
-        numberOfGuests.value + 1 
+    payload.value.guests = operation === AddOrSubtractGuest.ADD ? 
+    payload.value.guests + 1 
         : 
-        numberOfGuests.value > 0 ? 
-            numberOfGuests.value - 1
+        payload.value.guests > 0 ? 
+            payload.value.guests - 1
             :
             0
 }
+
+const performSearch = () => {
+    console.log(payload.value);
+}
+
 </script>
 
 <template>
@@ -35,16 +66,17 @@ const handleAddOrSubtractGuest = (operation: string) => {
             <div class="flex justify-around items-center bg-white p-2 w-[35%] rounded-full mr-3 h-[70%] border-2 border-gray-200">
                 <GlobeAsiaAustraliaIcon class="h-8 w-8 text-orange-600"/>
                 <input 
+                v-model="payload.location"
                 type="text" 
-                placeholder="Where to?" 
-                class="bg-transparent w-[80%] ml-3 focus-visible:outline-none h-full text-md"
+                placeholder="Where to?"
+                class="bg-transparent w-[80%] ml-3 focus-visible:outline-none h-full text-md text-gray-500"
                 >
             </div>
             <!-- TICKET #11 -->
             <button 
             class="group flex justify-center items-center bg-white p-2 w-[35%] rounded-full mr-3 h-[70%] transition-all border-2 border-gray-200"
             >
-                <DatePicker />
+                <DatePicker @updateDatedRange="updateDatedRange" />
             </button>
             <button 
             class="group flex justify-center items-center bg-white p-2 w-[17%] rounded-full mr-3 h-[70%] hover:bg-orange-600 transition-all border-2 border-gray-200 hover:border-0"
@@ -52,9 +84,10 @@ const handleAddOrSubtractGuest = (operation: string) => {
             >
                 <UserPlusIcon class="h-6 w-6 text-orange-600 group-hover:text-white"/>
                 <p class="text-gray-400 ml-3 group-hover:text-white">Guests</p>
-                <p v-if="numberOfGuests > 0" class="text-gray-500 ml-2 group-hover:text-white"> {{ numberOfGuests }}</p>
+                <p v-if="payload.guests > 0" class="text-gray-500 ml-2 group-hover:text-white"> {{ payload.guests }}</p>
             </button>
             <button 
+            @click="performSearch"
             class="group flex justify-center items-center hover:bg-orange-400 p-2 w-[50px] h-[50px] rounded-full mr-3  bg-orange-600 transition-all"
             >
                 <MagnifyingGlassIcon class="h-6 w-6 text-white"/>
@@ -74,7 +107,7 @@ const handleAddOrSubtractGuest = (operation: string) => {
                     >
                         <MinusIcon class="w-3 h-3 text-white" />
                     </button>
-                    <p class="ml-2 mr-2 w-10 text-center">{{ numberOfGuests }}</p>
+                    <p class="ml-2 mr-2 w-10 text-center">{{ payload.guests }}</p>
                     <button 
                     class="bg-orange-500 rounded-r-full h-full p-2"
                     @click="handleAddOrSubtractGuest(AddOrSubtractGuest.ADD)"
