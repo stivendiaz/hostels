354007db2035cee1525cd7270a949e077dfa92a9
stivendiaz <stivendiazagudelo@gmail.com>
2023-06-09 08:17:24 -0500

    header depeneding on session status (#66)

--- frontend/src/components/Property/Form/CreateProperetyForm.vue
+++ frontend/src/components/Property/Form/CreateProperetyForm.vue
@@ -76,55 +76,58 @@
     </div>
 
     <div class="flex flex-wrap">
-      <div class="w-full md:w-1/2 py-2 p-1">
+      <div class="w-full md:w-1/2 p-1">
+        <div class="flex flex-row">
+          <label
+            class="block text-gray-400 text-xs font-medium mb-2"
+            for="address"
+          >
+            Country
+          </label>
+          <label
+            v-if="selectedCountry.length === 0"
+            class="block text-red-700 text-xs font-light mb-2 pl-0.5"
+          >
+            *
+          </label>
+        </div>
+        <select
+          id="country"
+          v-model="selectedCountry"
+          @change="getCities"
+          class="w-full md:w-1/1 p-1"
+        >
+          <option value="">Select a country</option>
+          <option
+            v-for="country in countries"
+            :value="country.name"
+            :key="country.alpha2Code"
+          >
+            {{ country.name.common }}
+          </option>
+        </select>
+      </div>
+      <div class="w-full md:w-1/2 p-1" v-if="selectedCountry">
         <div class="flex flex-row">
           <label
             class="block text-gray-400 text-xs font-medium mb-2"
             for="city"
           >
-            city
+            City
           </label>
-
           <label
-            v-if="city.length === 0"
+            v-if="selectedCity.length === 0"
             class="block text-red-700 text-xs font-light mb-2 pl-0.5"
           >
             *
           </label>
         </div>
-
-        <input
-          v-model="city"
-          class="border-gray-300 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs h-[45px] placeholder:text-gray-300"
-          id="city"
-          type="text"
-          placeholder="example@example.com"
-          required
-        />
-      </div>
-      <div class="w-full md:w-1/2 py-2 p-1">
-        <div class="flex flex-row">
-          <label
-            class="block text-gray-400 text-xs font-medium mb-2"
-            for="country"
-          >
-            country
-          </label>
-          <label
-            v-if="country.length === 0"
-            class="block text-red-700 text-xs font-light mb-2 pl-0.5"
-          >
-            *
-          </label>
-        </div>
-        <input
-          v-model="country"
-          class="border-gray-300 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs h-[45px] placeholder:text-gray-300"
-          id="country"
-          type="text"
-          placeholder="302000000"
-          required
-        />
+        <select id="city" v-model="selectedCity" class="w-full md:w-1/1 p-1">
+          <option value="">Select a city</option>
+          <option v-for="city in cities" :value="city" :key="city">
+            {{ city }}
+          </option>
+        </select>
       </div>
     </div>
 
@@ -249,7 +252,13 @@
 </template>
 
 <script lang="ts" setup>
+interface Country {
+  name: string;
+  alpha2Code: string;
+}
+
 import { ref } from 'vue';
+import { watchEffect } from 'vue/dist/vue';
 const description = ref('');
 const image = ref('');
 const zipCode = ref('');
@@ -259,6 +268,56 @@ const email = ref('');
 const name = ref('');
 const address = ref('');
 const phone = ref('');
+
+const selectedCountry = ref('');
+const selectedCity = ref('');
+
+const countries = ref<Country[]>([]);
+const cities = ref<string[]>([]);
+
+const fetchCountries = async () => {
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    countries.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchCities = async (country: string) => {
+  console.log(JSON.stringify(country));
+  try {
+    const response = await fetch(
+      'https://countriesnow.space/api/v0.1/countries/cities',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ country: country.common }),
+      },
+    );
+    const data = await response.json();
+    cities.value = data.data.map((city: any) => city);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getCities = () => {
+  if (selectedCountry.value) {
+    fetchCities(selectedCountry.value);
+  } else {
+    cities.value = [];
+  }
+};
+
+watchEffect(() => {
+  getCities();
+});
+
+fetchCountries();
 
 function handleSubmit() {}
 </script>
